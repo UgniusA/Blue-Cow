1f913897a1b1f6d0ddeb1d76eac11ee578370de2
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-02-05 11:27:02 +0000

    asdasd

---
+++ Blue Cow/Assets/Scripts/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    [SerializeField] int speedBoost;
+    [SerializeField] int jumpBoost;
+    [SerializeField] int healthBoost;
+    [SerializeField] int immortalBoost;
+}
