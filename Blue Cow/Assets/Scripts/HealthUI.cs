33b8b253bd8d050d241a9be2d8a913df5fe236a9
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-02-10 10:03:03 +0000

    bug fix

--- Blue Cow/Assets/Scripts/HealthUI.cs
+++ Blue Cow/Assets/Scripts/HealthUI.cs
@@ -16,7 +16,7 @@ public class HealthUI : MonoBehaviour {
     Health healthScript;
 
     // Start is called before the first frame update
-    void Start() {
+    void Awake() {
         healthScript = GetComponent<Health>();
     }
 
