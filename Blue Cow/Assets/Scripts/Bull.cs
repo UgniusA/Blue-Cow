46059715b1f6a847310e99f7ca2414cd50916eae
BLC-88 <72603083+BLC-88@users.noreply.github.com>
2021-02-19 11:12:15 +0000

    asd

--- Blue Cow/Assets/Scripts/Bull.cs
+++ Blue Cow/Assets/Scripts/Bull.cs
@@ -14,6 +14,8 @@ public class Bull : MonoBehaviour {
     [SerializeField] float chargeResetTime = 1f;
     [SerializeField] SpriteRenderer sr;
 
+    int moveDir;
+    BoxCollider2D col;
     Rigidbody2D rb;
     PlayerController player;
 
@@ -21,6 +23,7 @@ public class Bull : MonoBehaviour {
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<PlayerController>();
+        col = GetComponent<BoxCollider2D>();
         canCharge = true;
     }
 
@@ -31,26 +34,35 @@ public class Bull : MonoBehaviour {
                 Vector2 dist = player.transform.position - transform.position;
                 if (dist.magnitude < sightRange) {
                     sr.flipX = dist.x > 0;
-                    float dir = dist.x < 0 ? -1 : 1;
-                    StartCoroutine(Charge(dir));
+                    moveDir = dist.x < 0 ? -1 : 1;
+                    StartCoroutine(Charge(moveDir));
                 }
-                else {
-                    rb.velocity = Vector2.zero;
-                }
+            }
+        }
+        if (isCharging) {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * moveDir, col.bounds.extents.x + 0.01f);
+            if (hit.collider.gameObject == player) {
+                Debug.Log("hit");
+                player.GetComponent<Health>().Damage(damage);
+                //rb.velocity = Vector2.zero;
+                StopCharge();
             }
         }
     }
 
     IEnumerator Charge(float dir) {
-        if (!isCharging) {
-            canCharge = false;
-            isCharging = true;
-            rb.velocity = Vector2.right * dir * chargeSpeed;
-            yield return new WaitForSeconds(chargeTime);
-            isCharging = false;
-            StartCoroutine(WaitTillCharge());
-        }
+        canCharge = false;
+        isCharging = true;
+        rb.velocity = Vector2.right * dir * chargeSpeed;
+        Debug.Log(Vector2.right * dir * chargeSpeed);
+        yield return new WaitForSeconds(chargeTime);
+        StopCharge();
         yield break;
+    }
+
+    void StopCharge() {
+        isCharging = false;
+        StartCoroutine(WaitTillCharge());
     }
 
     IEnumerator WaitTillCharge() {
@@ -58,11 +70,11 @@ public class Bull : MonoBehaviour {
         canCharge = true;
         yield break;
     }
-
+    /*
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject == player.gameObject) {
             player.GetComponent<Health>().Damage(damage);
             rb.velocity = Vector2.zero;
         }
-    }
+    }*/
 }
